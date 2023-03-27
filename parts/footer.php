8730fd45da9ef3aa13db126650254dab183a8d25
patrickt00 <patryk.trzaska4@gmail.com>
2023-03-27 03:20:24 +0200

    changed footer to non-sticky

--- parts/footer.php
+++ parts/footer.php
@@ -1,5 +1,5 @@
 <div class="container">
-<footer class="d-flex fixed-bottom flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
 
     <ul class="nav col-md-11 justify-content-end">
       <li class="nav-item"><a href="/" class="nav-link px-2 text-body-secondary">Strona główna</a></li>
